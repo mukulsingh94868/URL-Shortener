dfe9beedd11ff9f11748eeeac2d8d68cb21ea9ce
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-07-18 19:48:42 +0530

    use fetch custom hook and login component with integration

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,7 +1,32 @@
-const Auth = () => {
-  return (
-    <div>Auth</div>
-  )
-}
+import Login from "@/components/login";
+import Signup from "@/components/signup";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
-export default Auth
+const Auth = () => {
+  let [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  return (
+    <div className="mt-36 flex flex-col items-center gap-10">
+      <h1 className="text-5xl font-extrabold">
+        {searchParams.get("createNew")
+          ? "Hold up! Let's login first.."
+          : "Login / Signup"}
+      </h1>
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="signup">Signup</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <Login />
+        </TabsContent>
+        <TabsContent value="signup">
+          <Signup />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default Auth;
