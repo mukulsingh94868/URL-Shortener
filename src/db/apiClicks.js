45f7b91baae5e6adfd9a28d7be86005760a3b423
@Mukul <mukul.singh@zentekinfosoft.com>
2024-07-25 18:10:29 +0530

    Adding Row Level Security and Fetch Url and Clicks API

---
+++ src/db/apiClicks.js
@@ -0,0 +1,11 @@
+import supabase from "./supabase";
+
+export async function getClicksForUrls(urLIds) {
+    const { data, error } = await supabase.from("clicks").select("*").eq("utl_id", urLIds)
+
+    if (error) {
+        console.log('error', error.message)
+        throw new Error("Unable to load Clicks");
+    }
+    return data;
+}
