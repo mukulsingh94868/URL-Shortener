5732d71395097e414159d5d9198c0fd8cae17ede
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-07-27 21:28:12 +0530

    create url fron backend and forntend integration

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -15,6 +15,7 @@ import useFetch from "@/hooks/use-fetch";
 import { getUrls } from "@/db/apiUrls";
 import { getClicksForUrls } from "@/db/apiClicks";
 import { UrlState } from "@/context";
+import { CreateLink } from "@/components/create-link";
 
 const Dashboard = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -66,7 +67,7 @@ const Dashboard = () => {
       </div>
       <div className="flex justify-between">
         <h1 className="text-4xl font-extrabold">My Links</h1>
-        {/* <CreateLink /> */}
+        <CreateLink />
       </div>
       <div className="relative">
         <Input
