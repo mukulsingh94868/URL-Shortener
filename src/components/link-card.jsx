8d4968ed43d4df92eb7cd1d7348f42fcef60990d
@Mukul <mukul.singh@zentekinfosoft.com>
2024-07-31 16:11:54 +0530

    add redirects file

--- src/components/link-card.jsx
+++ src/components/link-card.jsx
@@ -40,7 +40,7 @@ const LinkCard = ({ url = [], fetchUrls }) => {
           {url?.title}
         </span>
         <span className="text-2xl text-blue-400 font-bold hover:underline cursor-pointer">
-          https://trimrr.in/{url?.custom_url ? url?.custom_url : url.short_url}
+          https://main--trimrr-short-url.netlify.app/{url?.custom_url ? url?.custom_url : url.short_url}
         </span>
         <span className="flex items-center gap-1 hover:underline cursor-pointer">
           <LinkIcon className="p-1" />
@@ -54,7 +54,7 @@ const LinkCard = ({ url = [], fetchUrls }) => {
         <Button
           variant="ghost"
           onClick={() =>
-            navigator.clipboard.writeText(`https://trimrr.in/${url?.short_url}`)
+            navigator.clipboard.writeText(`https://main--trimrr-short-url.netlify.app/${url?.short_url}`)
           }
         >
           <Copy />
@@ -65,7 +65,7 @@ const LinkCard = ({ url = [], fetchUrls }) => {
         <Button
           variant="ghost"
           onClick={() => fnDelete().then(() => fetchUrls())}
-          //   disable={loadingDelete}
+        //   disable={loadingDelete}
         >
           {loadingDelete ? <BeatLoader size={5} color="white" /> : <Trash />}
         </Button>
